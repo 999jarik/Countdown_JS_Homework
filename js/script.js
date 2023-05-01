bfd449ea97345207a33db09be7676d9c3d81de47
Yaroslav Trofimenko <92098465+999jarik@users.noreply.github.com>
2023-05-02 01:03:13 +0300

    Summary

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@ var pause = document.querySelector('.nav__button-pause');
 var continueTimer = document.querySelector('.nav__button-continue');
 var clear = document.querySelector('.nav__button-clear');
 var timer = document.querySelector('.clock__watch');
+var timerMilli = document.querySelector('.millisec');
 var hour = 0;
 var min = 0;
 var sec = 0;
+var millisec = 0;
 var interval;
 
 function startTimer () {
-    sec++;
+    millisec++;
+    if (millisec === 1000) {
+        sec++;
+        millisec = 0;
+    }
     if (sec === 60) {
         min++;
         sec = 0;
@@ -19,13 +25,14 @@ function startTimer () {
         min = 0;
     }
     timer.innerHTML = `${hour<10?"0"+hour:hour}:${min<10?"0"+min:min}:${sec<10?"0"+sec:sec}`;
+    timerMilli.innerHTML = `${millisec < 10 ? "00" + millisec : millisec < 100 ? "0" + millisec : millisec}`;
 };
 
 function forStart() {
     start.classList.add('hidden');
     pause.classList.remove('hidden');
     clearInterval(interval);
-    interval = setInterval(startTimer, 1000);
+    interval = setInterval(startTimer, 1);
 };
 function forPause() {
     pause.classList.add('hidden');
@@ -36,7 +43,7 @@ function forContinue() {
     continueTimer.classList.add('hidden');
     pause.classList.remove('hidden');
     clearInterval(interval);
-    interval = setInterval(startTimer, 1000);
+    interval = setInterval(startTimer, 1);
 };
 function forClear() {
     continueTimer.classList.add('hidden');
@@ -46,7 +53,9 @@ function forClear() {
     hour = 0;
     min = 0;
     sec = 0;
+    millisec = 0;
     timer.innerHTML = `${hour<10?"0"+hour:hour}:${min<10?"0"+min:min}:${sec<10?"0"+sec:sec}`;
+    timerMilli.innerHTML = `${millisec < 10 ? "00" + millisec : millisec < 100 ? "0" + millisec : millisec}`;
 };
 
 start.addEventListener('click', forStart);
