1ba8633760b2c5579eeed6fb0e2736ecfbb255a4
Yaroslav Trofimenko <92098465+999jarik@users.noreply.github.com>
2023-05-01 19:45:16 +0300

    Summ

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var start = document.querySelector('.nav__button-start');
 var pause = document.querySelector('.nav__button-pause');
+var contin = document.querySelector('.nav__button-continue');
 var clear = document.querySelector('.nav__button-clear');
 
 start.classList.add('hidden');
 start.classList.remove('hidden');
+
